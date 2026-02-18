8c5e7499f1fc4f28fdfc6d4a2d191e6b29b21080
mohitchouhan1110 <mohitchouhan.two@gmail.com>
2026-02-18 22:53:59 +0530

    Update database.js

--- Backend/src/config/database.js
+++ Backend/src/config/database.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const connectDB = async () => {
     try {
         
-        await mongoose.connect("mongodb+srv://mc3984925:monu1011@mohitnode.ilceiab.mongodb.net/?appName=MohitNode");
+        await mongoose.connect("your_mongodb_uri");
         // console.log("MongoDB connected");
     } catch (err) {
         console.error("MongoDB connection error:", err)
